cc7edbcc6d393664f8317af9cff2135cb7158fb0
hallelujahdrive <hallelujahdrive@gmail.com>
2018-10-05 15:06:08 +0900

    動作未検証.ソースがすごく汚い

--- popup/menu.js
+++ popup/menu.js
@@ -9,9 +9,49 @@ document.getElementById("menu_link").addEventListener("click", function(e) {
 });
 
 document.getElementById("menu_login").textContent = browser.i18n.getMessage("loginT2Wireless");
+document.getElementById("menu_login").addEventListener("click", function(e) {
+  let getting = browser.storage.local.get();
+  getting.then(onGot, onError);
+});
+
 document.getElementById("menu_option").textContent = browser.i18n.getMessage("optionsTitle");
 document.getElementById("menu_option").addEventListener("click", function(e) {
   browser.tabs.create({
     url: "../options.html"
   });
 });
+
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function openOptions (message) {
+  if (window.confirm(message)) {
+      // do something.
+    browser.tabs.create({
+        url: "../options.html"
+    });
+  }
+}
+
+function onGot(items) {
+  let account = items.account;
+  let password = items.password;
+    
+  if (account == null || account == "" || password == null || password == "") {
+  // 設定されていなければ設定画面を表示
+    openOptions (browser.i18n.getMessage("accountAndPasswordConfirmMessage"));
+  } else {
+      var queue = "username=" + account + "&" + "password=" + password + "&" + "buttonClicked=4";
+
+      var xhr = new XMLHttpRequest ();
+      xhr.open("POST", T2WirelessURL);
+      xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded;charset=UTF-8");
+      xhr.onreadystatechange = function() {
+ 
+            alert(xhr.responseText);
+      }
+      xhr.send(queue)
+    }
+}
